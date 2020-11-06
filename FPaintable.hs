2e5810e9fe1720e04dc1559593feb7f2da1d95e6
Eric D Culver <eric.d.culver@gmail.com>
2020-11-06 16:06:21 -0700

    basic structure for everything but painter in Haskell

--- FPaintable.hs
+++ FPaintable.hs
@@ -4,18 +4,30 @@ import Control.Monad(filterM)
 
 type Parts = [Int]
 
+type Choice = [[Int]]
+
 type FVals = [[Int]]
 
 check :: Parts -> FVals -> Bool
 check ps fs = all (\(p,f) -> p == length f) $ zip ps fs
 
 subsets :: Int -> [[Int]]
-subsets n = filterM (\x -> [True, False]) [1..n]
+subsets n = filterM (\x -> [False, True]) [1..n]
 
-lister :: Parts -> [[[Int]]]
-lister ps = map subsets ps -- not quite right
+lister :: Parts -> [Choice]
+lister ps = filter (any (/=[])) $ sequence $ map subsets ps
+
+painter :: Parts -> FVals -> Choice -> [(Parts, FVals)]
+painter ps fs cs = []
+
+fPaintable' :: Parts -> FVals -> Bool
+fPaintable' [] [] = True
+fPaintable' (0:ps) ([]:fs) = fPaintable' ps fs
+fPaintable' ps fs = all (any (uncurry fPaintable') . painter ps fs) $ lister ps
 
 fPaintable :: Parts -> FVals -> Maybe Bool
+fPaintable [] [] = Just True
+fPaintable (0:ps) ([]:fs) = fPaintable ps fs
 fPaintable ps fs = case check ps fs of
                         False -> Nothing
-                        True -> Just True
+                        True -> Just $ all (any (uncurry fPaintable') . painter ps fs) $ lister ps
