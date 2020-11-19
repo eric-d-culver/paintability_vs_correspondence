13a760b99e3a7077fd4d348c34b4d074589cc75e
Eric D Culver <eric.d.culver@gmail.com>
2020-11-19 16:39:33 -0700

    changes

--- f_paintability_multipartite.py
+++ f_paintability_multipartite.py
@@ -17,71 +17,72 @@ def leqList(l):
         yield k
 
 def lister(fvals):
-    for t in it.product(*map(leqList, l)):
+    for t in it.product(*map(leqList, fvals)):
         # need to check for empty choice (not allowed)
         if all(sum(l) == 0 for l in t):
             continue
         # otherwise it is fine
         yield t
 
+def decNotPicked(k, l):
+    return tuple(k[i] - l[i] + l[i+1] if i < len(k)-1 else k[i] - l[i] for i in range(len(k)))
+
+def decPicked(k, l):
+    return tuple(k[i] - l[i] for i in range(len(k)))
+
 def painter(fvals, lst):
     for i in range(len(fvals)):
         # can't pick this part if there are no vertices in it
-        if len(lst[i]) == 0:
+        if sum(lst[i]) == 0:
             #print(indent, "No vertices in this part to pick")
             continue
-        new_fvals = [list(x) for x in fvals]
-        # verts lister picked in this part are removed
-        new_fvals[i] = [x for (j,x) in enumerate(fvals[i]) if j not in lst[i]]
-        # verts lister picked in other parts have f values reduced by 1
-        for j in range(len(fvals)):
-            if j == i:
-                continue
-            for k in lst[j]:
-                new_fvals[j][k] -= 1
-        for j in range(len(fvals)):
-            new_fvals[j].sort()
-        tup_fvals = tuple([tuple(x) for x in new_fvals])
-        yield tup_fvals
+        yield tuple(decNotPicked(fvals[j], lst[j]) if j != i else decPicked(fvals[j], lst[j]) for j in range(len(fvals)))
 
 @lru_cache()
-def f_paintable_multipartite(fvals):
-    #print(indent, "fvals = ", fvals)
+def f_paintable_multipartite(fvals, indent='', verbose=False):
+    if verbose: print(indent, "fvals = ", fvals)
+    # if a vertex has higher f value than its degree, we can delete it
+    # (we can paint by only picking it if none of its neighbors are picked,
+    # since f value > degree, this is guarenteed to happen. So it can be colored,
+    # and we can ignore it.)
+    n = sum(sum(l) for l in fvals) # number of vertices in graph
+    while any(len(l) > n-sum(l)+1 for l in fvals): # recursively delete vertices until all f values at most degrees
+        if verbose: print(indent, "Removing vertices with f value greater than degree")
+        fvals = tuple(l[:n-sum(l)+1] for l in fvals) # +1 to allow f value to equal degree
+        n = sum(sum(l) for l in fvals) # update number of vertices in graph
+        if verbose: print(indent, "New fvals =", fvals)
     # if there are no more vertices, then we can trivially color
-    if len(fvals) == 0 or all(len(x) == 0 for x in fvals):
-        #print(indent, "Trivial color")
+    if len(fvals) == 0 or all(sum(x) == 0 for x in fvals):
+        if verbose: print(indent, "Trivial color")
         return True
-    # if any value in fvals is 0, we cannot color
-    if any(x == 0 for l in fvals for x in l):
-        #print(indent, "Can't color")
+    # if any vertices have an f value of 0, we cannot color
+    if any(l[0] > 0 for l in fvals):
+        if verbose: print(indent, "Can't color")
         return False
     # otherwise lister picks a subset
     for lst in lister(fvals):
-        #print(indent, "Lister picks: ", lst)
+        if verbose: print(indent, "Lister picks: ", lst)
         val = False
         # painter picks an independent set (assume it is all of some part)
         for new_fvals in painter(fvals, lst):
-            #print(indent, "Painter tries:", new_fvals)
+            if verbose: print(indent, "Painter tries:", new_fvals)
             # recursively call function on new smaller graph
             # if it is paintable, then painter has a response to this choice of lister
             # no need to continue checking
-            if f_paintable_multipartite(new_fvals):
-                #print(indent, "Painter succeeds with:", new_fvals)
+            if f_paintable_multipartite(new_fvals, indent=indent+"\t", verbose=verbose):
+                if verbose: print(indent, "Painter succeeds with:", new_fvals)
                 val = True
                 break
         # if painter has no good response, then lister wins
         if not val:
-            #print(indent, "Lister wins this one")
+            if verbose: print(indent, "Lister wins this one")
             return False
     # if painter has a good response to all of lister's choices, painter wins
-    #print(indent, "Painter wins this one")
+    if verbose: print(indent, "Painter wins this one")
     return True
 
 def k_for_all_verts(k, parts):
-    res = []
-    for p in parts:
-        res.append(tuple(it.repeat(k,p)))
-    return tuple(res)
+    return tuple(map(lambda x: (0,)*k + (x,), parts))
 
 def paintability(parts, kmax):
     for k in range(2, kmax):
@@ -97,4 +98,4 @@ def test_k(k, imin, imax, jmin, jmax):
             print(f_paintable_multipartite(k_for_all_verts(k, parts)))
 
 
-print(paintability((4,6),5))
+test_k(3, 2, 8, 2, 8)
